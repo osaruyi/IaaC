86f0d5872bd21b4b2ccebd3b5a1dd578c05ae160
Gabriel Uyi Omorogbe-Amu <omorogbe_g@yahoo.co.uk>
2019-02-06 14:54:52 +0000

    added count parameter

--- ec2instances.tf
+++ ec2instances.tf
@@ -1,10 +1,11 @@
 #Define webserver inside public subnet
 resource "aws_instance" "uyi_webserver" {
-  ami           = "ami-0b0a60c0a2bd40612" # eu-west-2b
-  instance_type = "t2.micro"
-  key_name = "  "
-  subnet_id = "${aws_subnet.uyipubsubnet.id}"
-  vpc_security_group_ids = ["${aws_security_group.uyi_websecgrp.id}"]
+  count                       = "2" #using the count parameter
+  ami                         = "ami-0b0a60c0a2bd40612" # eu-west-2b
+  instance_type               = "t2.micro"
+  key_name                    = " "
+  subnet_id                   = "${aws_subnet.uyipubsubnet.id}"
+  vpc_security_group_ids      = ["${aws_security_group.uyi_websecgrp.id}"]
   associate_public_ip_address = true
   source_dest_check = false
 
@@ -24,11 +25,12 @@ resource "aws_instance" "uyi_webserver" {
 
 #Define webserver inside private subnet
 resource "aws_instance" "uyi_dbserver" {
-  ami           = "ami-0b0a60c0a2bd40612" # eu-west-2c
-  instance_type = "t2.micro"
-  key_name = "  "
-  subnet_id = "${aws_subnet.uyiprisubnet.id}"
-  vpc_security_group_ids = ["${aws_security_group.uyi_dbsecgrp.id}"]
+  count                   = "2" # using the count parameter
+  ami                     = "ami-0b0a60c0a2bd40612" # eu-west-2c
+  instance_type           = "t2.micro"
+  key_name                = " "
+  subnet_id               = "${aws_subnet.uyiprisubnet.id}"
+  vpc_security_group_ids  = ["${aws_security_group.uyi_dbsecgrp.id}"]
   source_dest_check = false
 
   tags {
